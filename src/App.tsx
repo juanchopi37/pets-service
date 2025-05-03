dbf2b254ff0c759d92e57d1e2b79b39d760951bb
Juancho <cupinacop@proton.me>
2025-05-02 19:39:49 -0500

    Primer cambio en la rama

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Stack } from "@mui/material";
+import "./App.css";
+import {Route, Routes, Navigate} from "react-router-dom";
+imporrt { Loyaut } from "./components/Loyaut";
+
+function App() {
+ const [] 
+  
+  return (
+  <Loyaut>
+
+      <Routes>
+
+
+      </Routes>
+
+    </Loyaut>
+  )
+
+export default App;
