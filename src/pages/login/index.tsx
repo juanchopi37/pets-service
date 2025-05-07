f86ecdbe3892b2104b98fc79bad3e45feb9bef4a
joserivera-25 <josseerivera1@gmail.com>
2025-05-06 20:28:27 -0500

    cambios

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import   { useState } from 'react';
  export const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
